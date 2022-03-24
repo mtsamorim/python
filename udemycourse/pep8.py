612ed527cfe4c9f3fdc4320bb70450af6c305d8c
Matheus Amorim <40010844+mtsamorim@users.noreply.github.com>
2022-03-24 13:40:18 -0300

    pep8

---
+++ udemycourse/pep8.py
@@ -0,0 +1,35 @@
+"""
+PEP8 - Python Enhancement Proposal
+
+São Propostas de melhoria para a linguagem pyhton
+
+The Zen of Python
+
+import this
+
+[1] - Utilize Camel Case para nomes de classe;
+
+[2] - Utilize nomes em minúsculo, separados de underline para funções ou variáveis;
+
+
+"""
+
+class Calculadora:
+    pass
+
+
+class CalculadoraCientifica:
+    pass
+
+
+def soma():
+    pass
+
+
+def soma_dois():
+    pass
+
+
+numero = 4
+
+numero_impar = 5
