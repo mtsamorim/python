d3f8db02752b7c808fbe2b545d77fddc0b0c6c1f
Matheus Amorim <40010844+mtsamorim@users.noreply.github.com>
2022-03-25 10:14:24 -0300

    tipo_numerico

--- udemycourse/tipo_numerico.py
+++ udemycourse/tipo_numerico.py
@@ -2,10 +2,20 @@
 Tipo Numérico
 """
 
-num = 2
+num = input()
+num2 = int(input())
+num3 = input()
 
 print(num)
 
-print(num**num)
+print(num2**num2)
 
 print(1_000_000)
+
+print(type(32))
+
+print('Type var num = {0}' .format(type(num)))
+
+print('Type var num2 = {0}' .format(type(num2)))
+
+print('Type var num3 = {0}' .format(type(int(num))))
