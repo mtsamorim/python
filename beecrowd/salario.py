d55f3f380e7772d297855304211f39c35c014e50
Matheus Amorim <40010844+mtsamorim@users.noreply.github.com>
2022-03-25 18:13:23 -0300

    salario

---
+++ beecrowd/salario.py
@@ -0,0 +1,10 @@
+number = int(input())
+
+hours = float(input())
+
+salary = float(input())
+
+aux = float(hours*salary)
+
+print('NUMBER = {0}' .format(number))
+print('SALARY = U$ {:.2f}' .format(aux))
