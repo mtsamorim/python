2f5fd0aaaaa19c1a70f9f5bbe797da94af72f8ba
Matheus Amorim <40010844+mtsamorim@users.noreply.github.com>
2022-03-25 16:25:01 -0300

    tipo_booleano

---
+++ udemycourse/tipo_booleano.py
@@ -0,0 +1,24 @@
+"""
+Tipo Booleano
+
+# Errado
+true, false
+
+# Certo
+True, False
+"""
+
+status = True
+status2 = False
+
+print(status)
+
+print(not(status))
+
+print(status or status2)
+
+print(status and status2)
+
+print(5 > 6)
+
+print(6 == 6)
