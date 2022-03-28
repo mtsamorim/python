925b4e54956ede78b80e4ceee25416fc0d83d60a
Matheus Amorim <40010844+mtsamorim@users.noreply.github.com>
2022-03-28 16:47:15 -0300

    condicionais

---
+++ udemycourse/condicionais_if_else_elif.py
@@ -0,0 +1,25 @@
+idade = int(input())
+
+# Certo
+if idade < 18:
+    print('Menor de Idade')
+elif idade == 18:
+    print('Tem 18 Anos')
+elif idade == 27:
+    print('Tem 27 Anos')
+else:
+    print('Maior de Idade')
+
+
+# Errado
+if idade < 18:
+    print('Menor de Idade')
+
+elif idade == 18:
+    print('Tem 18 Anos')
+
+elif idade == 27:
+    print('Tem 27 Anos')
+
+else:
+    print('Maior de Idade')
