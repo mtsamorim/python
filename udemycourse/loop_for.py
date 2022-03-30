ec72f81b5864c3032a31c1eb052930d1854320ce
Matheus Amorim <40010844+mtsamorim@users.noreply.github.com>
2022-03-30 11:23:34 -0300

    loopfor

---
+++ udemycourse/loop_for.py
@@ -0,0 +1,28 @@
+"""
+Loop for
+
+Loop -> Estrutura de repetição
+For -> Uma dessas Estruturas
+
+C ou Java
+
+for(int i = 0; i < 10; i++){
+    //execução do código
+}
+
+Python
+
+for item in iteravel:
+    //execução do código
+
+Utilizamos loops para iterar sobre sequências ou sobre valores iteráveis
+
+Exemplo de iteráveis:
+- String
+    nome = 'Geek University'
+- Lista
+    lista = [1, 3, 5, 7, 9]
+- Range
+    numeros = range(1, 10)
+
+"""
