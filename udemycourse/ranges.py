4e65da1be3c49c7f90518013dc08f1172b6afc92
Matheus Amorim <40010844+mtsamorim@users.noreply.github.com>
2022-03-30 17:55:15 -0300

    ranges

---
+++ udemycourse/ranges.py
@@ -0,0 +1,33 @@
+"""
+Ranges
+
+OBS:
+
+# Nao Funciona
+lista = range(0,10)
+
+# Funciona
+lista = list(range(0,10))
+"""
+
+# Exemplo 1 
+for i in range(10):
+    print(i)
+
+print('=======================')
+
+# Exemplo 2
+for i in range(0, 10):
+    print(i)
+
+print('=======================')
+
+# Exemplo 3
+for i in range(5, 50, 5):
+    print(i)
+
+print('=======================')
+
+# Exemplo 4
+for i in range(10, 0, -1):
+    print(i)
