bcb4252e0361fd4c40fc135322fe08b8bd2cbfc5
Matheus Amorim <40010844+mtsamorim@users.noreply.github.com>
2022-06-28 15:13:57 -0300

    .

---
+++ datascienceacademy/Cap03 (funcoes)/exe02.py
@@ -0,0 +1,9 @@
+# Exercício 2 - Crie uam função que receba uma string como argumento e retorne a mesma string em letras maiúsculas.
+# Faça uma chamada à função, passando como parâmetro uma string
+
+def upperw(arg):
+    return arg.upper()
+
+word = input("Escreva uma palavra: ")
+
+upperw(word)
