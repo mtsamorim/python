a50ceeff181848c2ca70682bbf0da043b0dd67d8
Matheus Amorim <40010844+mtsamorim@users.noreply.github.com>
2022-06-28 15:19:27 -0300

    .

--- datascienceacademy/Cap03 (funcoes)/exe03.py
+++ datascienceacademy/Cap03 (funcoes)/exe03.py
@@ -1,9 +1,9 @@
 # Exercício 3 - Crie uma função que receba como parâmetro uma lista de 4 elementos, adicione 2 elementos a lista e 
 # imprima a lista
 def listadd(arg):
-    fruit1 = input("Qual a 1° fruta você vai adicionar?")
+    fruit1 = input("Qual a 1° fruta você vai adicionar? ")
     fruit1 = fruit1.capitalize()
-    fruit2 = input("Qual a 2° fruta você vai adicionar?")
+    fruit2 = input("Qual a 2° fruta você vai adicionar? ")
     fruit2 = fruit2.capitalize()
     return arg.extend([fruit1, fruit2])
 
