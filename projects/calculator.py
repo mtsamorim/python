7cb354de28807f311597bc0307dc6346b6003001
Matheus Amorim <40010844+mtsamorim@users.noreply.github.com>
2022-06-28 16:56:35 -0300

    calculator

--- projects/calculator.py
+++ projects/calculator.py
@@ -1,2 +1,53 @@
-print("+++++++++++++++++++Python Calculator+++++++++++++++++++")
+def soma(x, y):
+    return x + y
 
+def sub(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x,y):
+    return x / y
+
+print("+++++++++++++++++++Python Calculator+++++++++++++++++++\n")
+
+opt = int(input("Seleciona o número da opção desejada: \n\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n\nDigite sua opção [1/2/3/4]: "))
+
+if(opt == 1 or opt == 2 or opt == 3 or opt == 4):
+    print("Número inválido") 
+    
+    num1 = input("\nDigite o primeiro número: ")
+    
+    num2 = input("\nDigite o segundo número: ")
+
+    ty  = False
+
+    for i in num1:
+        if i == ".":
+            ty = True
+
+    num1 = float(num1)
+    num2 = float(num2)
+
+    if ty == True:
+        if(opt == 1):
+            print("\n%.1f + %.1f = %.1f" %(num1, num2, soma(num1, num2)))
+        elif(opt == 2):
+            print("\n%.1f - %.1f = %.1f" %(num1, num2, sub(num1, num2)))
+        elif(opt == 3):
+            print("\n%.1f * %.1f = %.1f" %(num1, num2, mul(num1, num2)))
+        else:
+            print("\n%.1f / %.1f = %.1f" %(num1, num2, div(num1, num2)))
+    else:
+        if(opt == 1):
+            print("\n%.0f + %.0f = %.0f" %(num1, num2, soma(num1, num2)))
+        elif(opt == 2):
+            print("\n%.0f - %.0f = %.0f" %(num1, num2, sub(num1, num2)))
+        elif(opt == 3):
+            print("\n%.0f * %.0f = %.0f" %(num1, num2, mul(num1, num2)))
+        else:
+            print("\n%.0f / %.0f = %.0f" %(num1, num2, div(num1, num2)))
+
+else:
+    print("Opção Inválida")
