c3b855e9fa583e03620de810882147a1b3ba8ac3
Matheus Amorim <40010844+mtsamorim@users.noreply.github.com>
2022-06-28 12:00:42 -0300

    .

---
+++ datascienceacademy/Cap02/exe8.py
@@ -0,0 +1,5 @@
+# Exercício 8 - Crie um dicionário com 3 chaves e 3 valores. Um dos valores deve ser uma lista de 2 elementos numéricos. 
+# Imprima o dicionário na tela.
+dict1 = {"list1":["pao", "queijo", "presunto"], "list2":["martelo", "pá", "balde"], "list3":[34, 56, 12]}
+print(dict1)
+print(len(dict1))
---
+++ datascienceacademy/Cap02/exe9.py
@@ -0,0 +1,10 @@
+# Exercício 9 - Crie uma lista de 4 elementos. O primeiro elemento deve ser uma string, 
+# o segundo uma tupla de 2 elementos, o terceiro um dcionário com 2 chaves e 2 valores e 
+# o quarto elemento um valor do tipo float.
+# Imprima a lista na tela.
+tupla1 = (1, "cow")
+dictfinal = {"zebra":23, "bear":67}
+listend = ["putfire", tupla1, dictfinal, 456.78]
+#or listend = ['putfire', (1, 'cow'), {'zebra': 23, 'bear': 67}, 456.78]
+print(listend)
+print(len(listend))
